df9daa060420cb211bd67975fdd77336b7ebedf2
“IshanG724” <ishang724@gmail.com>
2022-10-26 22:50:52 +0530

    First try with input along with calc simple interest

---
+++ src/com/company/SimpleInterest.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner ip =new Scanner(System.in);
+//        System.out.println("Enter your string : ");
+//        String st1=ip.nextLine();
+//        System.out.println(st1);
+//        System.out.print("Enter principal amount: ");
+//        float principal= ip.nextFloat();
+//        System.out.print("Enter rate : ");
+//        float rate= ip.nextFloat();
+//        System.out.print("Enter time : ");
+//        float time= ip.nextFloat();
+//        System.out.println("Total Amount : "+(principal+(principal*rate*time/100)));
+
+    }
+}
